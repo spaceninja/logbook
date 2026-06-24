e613de1b739d919a9f5c53e515d1d03f4298556e
Scott Vandehey <fake-scott@spaceninja.com>
2026-06-23 20:15:09 -0700

    basic page setup

---
+++ app/error.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>{{ error.status }}</h1>
+    <NuxtLink to="/">Go back home</NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { NuxtError } from '#app';
+
+defineProps<{ error: NuxtError }>();
+</script>
